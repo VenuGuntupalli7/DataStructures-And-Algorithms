c1bd63601f37fbbb33713699d5f2856dd0eb11ab
VenuGuntupalli7 <151303903+VenuGuntupalli7@users.noreply.github.com>
2023-11-28 10:27:19 -0800

    Bubble sort algorithm easy peasy

---
+++ src/LeetCode/algorithms/BubbleSort.java
@@ -0,0 +1,36 @@
+package LeetCode.algorithms;
+
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {3,1,4,5,2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+
+        boolean swapped;
+        // run the steps n-1 times
+        for(int i=0; i<arr.length; i++){
+            swapped =false;
+            //for each step, the maximum item will come at the last respective index
+            for(int j=1; j<arr.length-i; j++){
+                //swap it if the item is less than previous element
+                if(arr[j] < arr[j-1]){ //1<3
+                    //swap it by using our swapping methodology
+                    int temp = arr[j]; // temp:1
+                    arr[j] = arr[j-1]; // arr[j]: 3
+                    arr[j-1] = temp;   // arr[j-1]: 1
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+
+    }
+}
--- src/LeetCode/binarySearch/CeilingOfNumber.java
+++ src/LeetCode/binarySearch/CeilingOfNumber.java
@@ -3,7 +3,7 @@ package LeetCode.binarySearch;
 public class CeilingOfNumber {
     public static void main(String[] args){
         int[] array = {1,3,5,6,9,14,16,18};
-        int target = 7;
+        int target = 9;
         int result = ceilingNumber(array, target);
         System.out.println(result);
     }
