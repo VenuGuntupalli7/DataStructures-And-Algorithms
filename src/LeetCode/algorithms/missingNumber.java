21917e04252f52ebafce19c4c0651e94faf0c2ea
vguntupalli <vguntupalli@ashion.com>
2023-12-07 00:21:05 -0800

    added the main class

--- src/LeetCode/algorithms/missingNumber.java
+++ src/LeetCode/algorithms/missingNumber.java
@@ -2,7 +2,7 @@ package LeetCode.algorithms;
 
 class missingNumber {
     public static void main(String[] args) {
-        int[] nums = {4,0,2,1};
+        int[] nums = {9,6,4,2,3,5,7,0,1};
         System.out.println(missingNumber(nums));
     }
     public static int missingNumber(int[] nums) {
