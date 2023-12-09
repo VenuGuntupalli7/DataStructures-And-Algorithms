7ebbd0200947cb020df48f55dfa2a32f28668513
vguntupalli <vguntupalli@ashion.com>
2023-12-09 14:53:55 -0800

    Preety Printing

---
+++ src/basics/fundamentals/PrettyPrinting.java
@@ -0,0 +1,12 @@
+package basics.fundamentals;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+
+        float number = 983.9859f;
+        System.out.printf("Formatted number is : %.3f", number);
+        System.out.printf("formatted PI: %.2f", Math.PI);
+
+        System.out.printf("Hello, my name is %s and I will be a %s employee", "Venu", "Tesla");
+    }
+}
--- src/basics/fundamentals/StringComparsion.java
+++ src/basics/fundamentals/StringComparsion.java
@@ -11,7 +11,7 @@ public class StringComparsion {
         String name3 = new String ("Venu");
         String name4 = new String("Venu");
         System.out.println(name3 == name4);  // this is false because even though we created two objects outside of String pool via using new keyword which
-                                             // can create objects in heap memory. but the == will only be true if the both reference variables are pointed to single object
+                                             // can create objects in heap memory. but the == will only be true if the bo th reference variables are pointed to single object
 
 
         System.out.println(name1.equals(name2));
