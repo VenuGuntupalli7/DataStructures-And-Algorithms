35e3f557c5858770c0db0687cfdf7fe46727a0d4
vguntupalli <vguntupalli@ashion.com>
2023-12-15 11:47:20 -0800

    Factorial Of N

--- src/fundamentals/Recursion/SmallProblems.java
+++ src/fundamentals/Recursion/SmallProblems.java
@@ -2,8 +2,8 @@ package fundamentals.Recursion;
 
 public class SmallProblems {                 //Space complexity: O(n) times, because we are storing n times each function calls and not taking any extra space
     public static void main(String[] args) { // Time complexity is : O(n) times, because we are printing n times,
-        //fiveToOne(5);
-        OnetoFive(5);
+        fiveToOne(5);
+        //OnetoFive(5);
     }
     public static void fiveToOne(int n){
         //Base condition in Recursion:
